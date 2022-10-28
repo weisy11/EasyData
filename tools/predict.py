f82fb89542a605e5c486120489cd104f9593d92a
root <root@bjyz-sys-gpu-kongming9.bjyz.baidu.com>
2022-10-28 07:42:06 +0000

    support to predict by pmp

---
+++ tools/predict.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved. 
+#   
+# Licensed under the Apache License, Version 2.0 (the "License");   
+# you may not use this file except in compliance with the License.  
+# You may obtain a copy of the License at   
+#   
+#     http://www.apache.org/licenses/LICENSE-2.0    
+#   
+# Unless required by applicable law or agreed to in writing, software   
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+# See the License for the specific language governing permissions and   
+# limitations under the License.
+
+import os
+import sys
+parent = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(parent, '../deploy/PaddleModelPipeline')))
+
+from pmp.engine.pipeline import Pipeline
+from pmp.utils.logger import setup_logger
+from pmp.core.config import ArgsParser
+
+
+def argsparser():
+    parser = ArgsParser()
+
+    parser.add_argument(
+        "--config",
+        type=str,
+        default=None,
+        help=("Path of configure"),
+        required=True)
+    parser.add_argument(
+        "--input",
+        type=str,
+        default=None,
+        help="Path of input, suport image file, image directory and video file.",
+        required=True)
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="output",
+        help="Directory of output visualization files.")
+    parser.add_argument(
+        "--run_mode",
+        type=str,
+        default='paddle',
+        help="mode of running(paddle/trt_fp32/trt_fp16/trt_int8)")
+    parser.add_argument(
+        "--device",
+        type=str,
+        default='cpu',
+        help="Choose the device you want to run, it can be: CPU/GPU/XPU, default is CPU."
+    )
+    return parser
+
+
+if __name__ == '__main__':
+    parser = argsparser()
+    FLAGS = parser.parse_args()
+    input = os.path.abspath(FLAGS.input)
+    pipeline = Pipeline(input, FLAGS)
+    pipeline.run()
