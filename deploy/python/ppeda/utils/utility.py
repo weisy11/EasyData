762d0469688336fbb187c1893dcbf16dcfaed828
xiaoting <31891223+tink2123@users.noreply.github.com>
2022-11-09 18:08:27 +0800

    Update utility.py

--- deploy/python/ppeda/utils/utility.py
+++ deploy/python/ppeda/utils/utility.py
@@ -55,6 +55,8 @@ def rm_repeat(all_label, save_list, compare_file, out_file, thresh, delimiter):
 
 
 def check_dir(path):
+    if os.path.isfile(path):
+        return
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
     return
